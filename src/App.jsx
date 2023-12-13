c5d76e6d832b2e688f283065f4d65963f31f83d8
Abhi-m-anue <125118501+Abhi-m-anue@users.noreply.github.com>
2023-12-14 00:01:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className='header'>
+        <h2>Logo</h2>
+       </div>
+
+       <div className='banner'>
+        <h1> an image on background and welcome to ec billboard text over it</h1>
+       </div>
+
+       <div className='body'>
+        <h2>Updates</h2>
+        <div className='content-container'>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Cum quidem laboriosam expedita accusantium mollitia? </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Cum quidem laboriosam expedita accusantium mollitia? </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Cum quidem laboriosam expedita accusantium mollitia? </p>
+        </div>
+       </div>
+       
+       <div className='footer'>
+        <h3>EC Association</h3>
+        <p>social media links</p>
+       </div>
+    </>
+  )
+}
+
+export default App
